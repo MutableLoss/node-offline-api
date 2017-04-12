388094e53c070c8b0cfdc3dae11ba56800ad19d9
Dennis Brown <vilesyn@gmail.com>
2017-04-12 16:17:36 -0500

    fix file reading error

--- app/index.js
+++ app/index.js
@@ -6,33 +6,36 @@ const arch = os.platform();
 
 const { exec, execFile } = require('child_process');
 const gen = '/app/doc/tools/generate.js';
-const dir = 'app/doc/api/';
+const dir = __dirname + '/doc/api/';
+let count = 0;
 
 console.log('Dir: %s', __dirname);
 
 var child;
-let flags = ['app/doc/tools/doc/generate.js', '--template=app/doc/template.html'];
-  
-if(process.argv[2]) {
-  flags.push(' --node-version=' + process.argv[2]); 
+let flags = [__dirname + '/doc/tools/doc/generate.js', '--template=' + __dirname + '/doc/template.html'];
+
+if (process.argv[2]) {
+  flags.push(' --node-version=' + process.argv[2]);
 }
 
 function convertFile(file) {
   // take file and generate html file
-  if(arch === 'darwin' || arch === 'linux') {
+  if (arch === 'darwin' || arch === 'linux') {
+    count++;
     let tempFlags = flags.slice(0);
-    tempFlags.push('app/doc/api/' + file);
+    tempFlags.push(__dirname + '/doc/api/' + file);
     execFile('node', tempFlags, (err, out, stderr) => {
-      if(err) throw err.stack;
+      if (err) console.log(err.stack);
       console.log('Reading file %s', tempFlags[2]);
       console.log(out);
     });
-  } else if(arch === 'win32') {
-    let bat = 'doc.bat';
-    let command = bat + flags.join(' ') + file;
+  } else if (arch === 'win32') {
+    const bat = 'doc.bat';
+    const command = bat + flags.join(' ') + file;
     exec(command, (err, out, stderr) => {
-      if(err) throw err;
-      console.log(out);
+      if (err) console.log(err);
+      if (stderr) console.log(stderr);
+      console.log('Creating HTML file: ' + file);
     });
   }
 }
@@ -41,16 +44,27 @@ function convertFile(file) {
 function checkFiles() {
   fs.readdir(dir, function(err, res) {
     var ext = path.extname('*.md');
-    if(err) console.error(err.stack);
+    if (err) console.error(err.stack);
     res.forEach((file) => {
-      if(path.extname(file) === ext) {
-        convertFile(file);
+      if (path.extname(file) === ext) {
+         convertFile(file);
       }
     });
   });
 }
 
-checkFiles();
+fs.stat('../build', (err, out) => {
+  if (err) console.log(err);
+  if (out) {
+    console.log('build folder exists');
+  } else {
+    console.log('creating build folder');
+    fs.mkdirSync('../build');
+  }
+});
+  
+// fs.mkdir()
+// checkFiles();
 
 // run
 //  check version arg
