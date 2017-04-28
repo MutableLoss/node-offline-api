c4016d64c541d7ffa515ff5527c8071a5063816e
Dennis Brown <vilesyn@gmail.com>
2017-04-28 13:34:37 -0500

    add version folder options

--- app/index.js
+++ app/index.js
@@ -2,23 +2,43 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const fsx = require('fs-extra');
-
 const arch = os.platform();
 
 const { spawn, exec, execFile } = require('child_process');
 const gen = '/app/doc/tools/generate.js';
 const dir = __dirname + '/doc/api/';
-const buildDir = __dirname.slice(0,-3) + 'build/';
-let count = 0;
-
-console.log('Dir: %s', __dirname);
+let buildDir = __dirname.slice(0,-3) + 'build/';
+let count = 0, argCount = 0;
 
 var child;
-let flags = [__dirname + '/doc/tools/doc/generate.js', '--template=' + __dirname + '/doc/template.html', '', '--maxBuffer=1024*500'];
+let flags = [__dirname + '/doc/tools/doc/generate.js', '--template=' + __dirname + '/doc/template.html', ''];
 
-if (process.argv[2]) {
-  flags.push(' --node-version=' + process.argv[2]);
-}
+const options = process.argv.slice(2);
+
+// Check for options
+options.forEach(function(ops) {
+  if (ops.startsWith('-f')) {
+    buildDir = process.argv[argCount + 3];
+    if(buildDir.slice(-1) !== '/') {
+       buildDir += '/';
+    }
+  } else if (ops.startsWith('-v')) {
+    let version = process.argv[argCount + 3];
+    flags[3] = '--node-version=' + version;
+  }
+  argCount++;
+});
+
+console.log('NODe API: Building to Directory: %s', buildDir);
+
+
+// console.log('options', process.argv);
+
+// if (process.argv[2]) {
+//   if (process.argv[2].startsWith('--')) {
+//     inputFile = arg;
+//    process.argv[2];
+// }
 
 function convertFile(file) {
   // take file and generate html file
@@ -29,25 +49,21 @@ function convertFile(file) {
     let pathname = buildDir + filename + '.html';
     const fileStream = fs.createWriteStream(pathname);
     const createFile = spawn('node', flags);
-    createFile.stderr.on('data', (err) => {
-      console.log(err.stack);
-    });
-    createFile.stdout.pipe(fileStream);
 
+    createFile.stdout.pipe(fileStream);
     createFile.on('close', (code) => {
       if (code !== 0) {
-        console.log(`process exited with code ${code}`);
+        console.log('NODe API: process exited with code ' + code);
+      } else {
+        console.log('NODe API: Creating doc: ' + filename);
       }
     });
-    createFile.on('finish', () => {
-      console.log('Stream closed!');
-    })
   } else if (arch === 'win32') {
     const bat = 'doc.bat';
     const command = bat + flags.join(' ') + file;
     exec(command, (err, out, stderr) => {
       if (err) console.log(err);
-      console.log('Creating HTML file: ' + file);
+      console.log('NODe API: Creating HTML file: ' + file);
     });
   }
 }
@@ -67,24 +83,22 @@ function checkFiles() {
 
 function copyAssets() {
   fsx.copy(__dirname + '/doc/api_assets', buildDir + 'assets/', (err) => {
-    if (err) return console.error(err);
-    console.log('Copy success!');
+    if (err) return console.error('copy', err);
+    console.log('NODe API: Copy success!');
   });
 }
 
 // create document directory and copy assets
 fs.stat(buildDir, (err, out) => {
-  if (err) console.log(err);
-  // console.log(out.isDirectory());
   if (out) {
-    console.log('build folder exists');
+    console.log('NODe API: build folder exists');
     fs.stat(buildDir + 'assets', (err, out) => {
       if (err) {
         copyAssets();
       }
     });
   } else {
-    console.log('creating build folder');
+    console.log('NODe API: creating build folder');
     fs.mkdirSync(buildDir);
     copyAssets();
   }
