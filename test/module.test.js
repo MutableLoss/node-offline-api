f020b1e0bc1e7a4f4f39ebf2b009f9d370b9327a
Dennis Brown <vilesyn@gmail.com>
2017-06-02 03:29:06 -0500

    test(tests): fix tests, add lockfile
    removed automatic folder remove from test, fixed to reflect new version, and added yarn lockfile for
    module versions

--- test/module.test.js
+++ test/module.test.js
@@ -3,14 +3,17 @@ var fs = require('fs');
 var fsx = require('fs-extra');
 var buildOptions = require('../app/index.js').buildOptions;
 
-// buildOptions.buildDir = '~/';
+buildOptions.buildDir = process.cwd();
+buildOptions.buildName = 'node-docs';
 
 describe('node-offline-api', () => {
   it('should create files', () => {
     var createDocs = require('../app/index').createDocs;
-    const build = fs.statSync(buildOptions.buildDir);
-    expect(build).to.satisfy;
-    fsx.remove(buildOptions.buildDir);
+    createDocs();
+    const build = fs.stat(`${buildOptions.buildDir}`, (err, out) => {
+      expect(build).to.satisfy;
+      expect(out).to.satisfy;
+    });
   })
   it('should change options', () => {
     buildOptions.version = '4.4.0';
